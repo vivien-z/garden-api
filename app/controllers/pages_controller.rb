9d3618d7518970e12abf9f60f090e9517bba1b84
vivien zhu <yuanzyy@gmail.com>
2021-05-21 11:05:14 -0400

    added route, view page, and navbar option for api instruction

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,7 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def api_instruction
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+
       resources :plants, only: [ :index, :show, :update, :create ]
     end
   end
 
   root to: 'pages#home'
+  get 'api' => 'pages#api_instruction'
 
   resources :plants, only: [:index, :show, :create]
 end
