a0d964d0598847ba08582bc1db565ae23376027f
vivien zhu <yuanzyy@gmail.com>
2021-06-23 14:26:03 -0400

    onclick copy token content functioning, btn change from Copy to Copied

--- app/javascript/components/onclick_to_clipboard.js
+++ app/javascript/components/onclick_to_clipboard.js
@@ -1,8 +1,21 @@
 const onclickToClipboard = () => {
+  const btn = document.getElementById("apiTokenBtn");
   const token = document.getElementById("apiToken");
-  token.select();
-  document.execCommand("copy");
-  console.log("test")
+  btn.addEventListener('click', (e) => {
+    if (document.selection) {
+      const range = document.body.createTextRange();
+      range.moveToElementText(token);
+      range.select().createTextRange();
+      document.execCommand("copy");
+    } else if (window.getSelection) {
+      const range = document.createRange();
+      range.selectNode(token);
+      window.getSelection().addRange(range);
+      document.execCommand("copy");
+    }
+    btn.innerHTML = "Copied";
+  });
+
 }
 
 export { onclickToClipboard };
