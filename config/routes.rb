b6e5712284d3619c3c644fbe84a57c6a4d64fb88
vivien zhu <yuanzyy@gmail.com>
2021-09-06 00:03:20 -0400

    in process: debug devise sign_out issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    delete 'sign_out', to: 'devise/sessions#destroy'
+  end
+  # devise_for :users do
+  #   get '/users/sign_out' => 'devise/sessions#destroy'
+  #   get '/users/:id' => 'users#show'
+  # end
+  # devise_for :users do
+  #   # get 'logout', to: "devise/sessions#destroy", as: "logout"
+  #   # get 'logout', to: "users/sessions#destroy", as: "logout"
+  #   get '/sign_out' => 'devise/sessions#destroy'
+  # end
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
@@ -9,9 +21,11 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
   get 'api' => 'pages#api_doc'
+# end
   # root to: 'plants#index'
 
   resources :users, only: [:show] do
+    # get '/sign_out' => "devise/sessions#destroy"
     resources :gardens, only: [ :index, :new, :create ]
   end
   resources :gardens, only: [:show, :edit, :update, :destroy]
