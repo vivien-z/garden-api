445e929e1db60e38bf51e70cbf9e13c56c7c8efb
vivien zhu <yuanzyy@gmail.com>
2021-09-10 23:58:26 -0400

    show selected data for slider in form

--- app/javascript/components/range_slider.js
+++ app/javascript/components/range_slider.js
@@ -1,41 +1,27 @@
 const rangeSlider = () => {
   const slider = $('.range-slider'),
-      form = $('.form-group'),
-      range = $('.range-slider__range'),
-      value = $('.range-slider__value');
-  // console.log(slider)
-  // console.log(range)
-    slider.on('input', function(){
-      console.log($(this).children('input'));
-      console.log(this.value);
-      $(this).next(value).html(this.value);
+        range = $('.range-slider__range'),
+        value = $('.range-slider__value');
+
+    // range.on('input', function(){
+    //   console.log($(this));
+    //   console.log($(this).parent().next().html(this.value));
+    //   console.log(this.value);
+    //   // $(this).next(value).html(this.value);
+    //   $(this).parent().next().html(this.value)
+    // });
+
+  slider.each(function(){
+
+    value.each(function(){
+      const value = $(this).prev().children('input').attr('value');
+      $(this).html(value);
+    });
+    range.on('input', function(){
+      $(this).parent().next(value).html(this.value);
     });
 
-
-
-  // slider.each(function(){
-  //   const currentSlider = $(this)
-  //   console.log(currentSlider.prev(value));
-  //   console.log(currentSlider.children[0])
-
-
-
-
-
-
-  //   value.each(function(){
-  //     const value = $(this).prev().attr('value');
-  //     // console.log($(this));
-
-  //     $(this).html(value);
-  //   });
-  //   range.on('input', function(){
-  //     console.log($(this).parent());
-  //     console.log(this.value);
-  //     $(this).next(value).html(this.value);
-  //   });
-
-  // });
+  });
 };
 
 export { rangeSlider };
