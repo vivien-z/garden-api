ca4a139e21a609983f1639774d74bac0c786a8ed
vivien zhu <yuanzyy@gmail.com>
2021-09-10 23:40:36 -0400

    added slider for my garden form

--- app/javascript/components/range_slider.js
+++ app/javascript/components/range_slider.js
@@ -1,19 +1,41 @@
 const rangeSlider = () => {
   const slider = $('.range-slider'),
+      form = $('.form-group'),
       range = $('.range-slider__range'),
       value = $('.range-slider__value');
-
-  slider.each(function(){
-
-    value.each(function(){
-      const value = $(this).prev().attr('value');
-      $(this).html(value);
-    });
-
-    range.on('input', function(){
+  // console.log(slider)
+  // console.log(range)
+    slider.on('input', function(){
+      console.log($(this).children('input'));
+      console.log(this.value);
       $(this).next(value).html(this.value);
     });
-  });
+
+
+
+  // slider.each(function(){
+  //   const currentSlider = $(this)
+  //   console.log(currentSlider.prev(value));
+  //   console.log(currentSlider.children[0])
+
+
+
+
+
+
+  //   value.each(function(){
+  //     const value = $(this).prev().attr('value');
+  //     // console.log($(this));
+
+  //     $(this).html(value);
+  //   });
+  //   range.on('input', function(){
+  //     console.log($(this).parent());
+  //     console.log(this.value);
+  //     $(this).next(value).html(this.value);
+  //   });
+
+  // });
 };
 
 export { rangeSlider };
