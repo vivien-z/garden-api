e0a332a0d5e1cf2b637c271bb2284dd9336fc036
vivien zhu <yuanzyy@gmail.com>
2021-09-16 16:36:23 -0400

    cleared and organized code

--- app/javascript/components/add_to_plant_detail.js
+++ app/javascript/components/add_to_plant_detail.js
@@ -14,16 +14,14 @@ const addToPlantDetailList = (draggedElmt) => {
         seedDate = divValueByClassName(draggedElmt, "plant-detail__seedDate") || 'n/a',
         transDate = divValueByClassName(draggedElmt, "plant-detail__transDate") || 'n/a';
 
-  const dropField = document.getElementsByClassName('drop-field').item(0)
+  const dropField = document.getElementsByClassName('drop-field').item(0),
+        table = document.getElementsByClassName("plant-detail__table")[0],
+        tableContent = table.getElementsByTagName('tbody')[0];
   let offX = draggedElmt.getBoundingClientRect().left - dropField.getBoundingClientRect().left,
       offY = draggedElmt.getBoundingClientRect().top - dropField.getBoundingClientRect().top;
 
-  const table = document.getElementsByClassName("plant-detail__table")[0]
-  const tableContent = table.getElementsByTagName('tbody')[0]
-  const plantName = draggedElmt.getElementsByTagName('a')[0].innerText
-
-  const plantPosition = `<tr><td>${name}</td><td>${light}</td><td>${size}</td><td>${indoorSeeding}</td><td>${seedDate}</td><td>${transDate}</td></tr>`
-  tableContent.insertAdjacentHTML('beforeend',plantPosition)
+  const plantInfo = `<tr><td>${name}</td><td>${light}</td><td>${size}</td><td>${indoorSeeding}</td><td>${seedDate}</td><td>${transDate}</td></tr>`
+  tableContent.insertAdjacentHTML('beforeend',plantInfo)
 }
 
 export { addToPlantDetailList };
