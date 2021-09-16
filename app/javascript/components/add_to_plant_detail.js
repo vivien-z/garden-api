a0d292e8bf33cd488e2731bce25873da171ce7bb
vivien zhu <yuanzyy@gmail.com>
2021-09-16 16:31:23 -0400

    pass data to plant detail table

--- app/javascript/components/add_to_plant_detail.js
+++ app/javascript/components/add_to_plant_detail.js
@@ -1,25 +1,28 @@
 const addToPlantDetailList = (draggedElmt) => {
 
-  function divValueByClassName(div, className) {
-    return div.getElementsByClassName(className)[0]
+  function divValueByClassName(elmt, className) {
+    const div = elmt.getElementsByClassName(className)[0]
+    if (div) {
+      return div.innerText
+    }
   }
   // plant info data variable
-  const name = divValueByClassName(draggedElmt, "plant-detail__name")
-  const light = divValueByClassName(draggedElmt, "plant-detail__light")
-  const size = divValueByClassName(draggedElmt, "plant-detail__size")
-  console.log(name)
-  console.log(light)
-  console.log(size)
+  const name = divValueByClassName(draggedElmt, "plant-detail__name"),
+        light = divValueByClassName(draggedElmt, "plant-detail__light"),
+        size = divValueByClassName(draggedElmt, "plant-detail__size"),
+        indoorSeeding = divValueByClassName(draggedElmt, "plant-detail__inSeeding") ? 'True' : 'Flase',
+        seedDate = divValueByClassName(draggedElmt, "plant-detail__seedDate") || 'n/a',
+        transDate = divValueByClassName(draggedElmt, "plant-detail__transDate") || 'n/a';
 
   const dropField = document.getElementsByClassName('drop-field').item(0)
-  let offX = draggedElmt.getBoundingClientRect().left - dropField.getBoundingClientRect().left;
-  let offY = draggedElmt.getBoundingClientRect().top - dropField.getBoundingClientRect().top;
+  let offX = draggedElmt.getBoundingClientRect().left - dropField.getBoundingClientRect().left,
+      offY = draggedElmt.getBoundingClientRect().top - dropField.getBoundingClientRect().top;
 
   const table = document.getElementsByClassName("plant-detail__table")[0]
   const tableContent = table.getElementsByTagName('tbody')[0]
   const plantName = draggedElmt.getElementsByTagName('a')[0].innerText
 
-  const plantPosition = `<tr><td>${plantName}</td><td>${plantName}</td><td>${plantName}</td><td>${'ture'}</td><td>${new Date()}</td><td>${new Date()}</td></tr>`
+  const plantPosition = `<tr><td>${name}</td><td>${light}</td><td>${size}</td><td>${indoorSeeding}</td><td>${seedDate}</td><td>${transDate}</td></tr>`
   tableContent.insertAdjacentHTML('beforeend',plantPosition)
 }
 
