e2e041dac492e111f4027f092823909243072a61
vivien zhu <yuanzyy@gmail.com>
2021-09-24 21:38:43 -0400

    add form for plant detail

--- app/controllers/plant_position_by_gardens_controller.rb
+++ app/controllers/plant_position_by_gardens_controller.rb
@@ -1,2 +1,17 @@
 class PlantPositionByGardensController < ApplicationController
+  def create
+    @plant_position_by_garden = PlantPositionByGarden.new
+    @garden = Garden.find(params[:garden_id])
+    @plant_position_by_garden.garden = @garden
+    @plant = Plant.find(params[:plant_id])
+    @plant_position_by_garden.plant = @plant
+
+    authorize(@plant_position_by_garden)
+
+    if @plant_position_by_garden.save
+      redirect_to garden_path(@garden), notice: "Plant Info added to garden successfully."
+    else
+      render garden_path(@garden), notice: "Please try again."
+    end
+  end
 end
