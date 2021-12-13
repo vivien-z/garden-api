220d05e4beaf0d3e78431c1e5c982ddb8979a767
vivien zhu <yuanzyy@gmail.com>
2021-12-13 11:26:38 -0500

    JS component: list plant position function. Bug: mousedownToDrag to be fixed

--- app/javascript/components/list_plant_position.js
+++ app/javascript/components/list_plant_position.js
@@ -1,27 +1,14 @@
 const listPlantPosition = () => {
+  const field = document.getElementsByClassName("garden-field")[0]
   // pass object data from Rails to Javascript
   const garden = $('.garden_information').data('garden')
   const positions = $('.garden_information').data('positions')
-  // const position = $('.position_information').data('position')
-  console.log(garden)
-  console.log(test)
-  // console.log(position)
 
-  const field = document.getElementsByClassName("garden-field")[0]
-  const plant = field.getElementsByClassName("plantCopy")[0]
-
-  console.log(plant)
-  // // plants.forEach ((plant) => {
-  if (plant) {
-    plant.style.left = position.positionX + "px" // getting position without using event
-    plant.style.top  = position.positionY + "px"
-    console.log(field.offsetLeft)
-    console.log(field.offsetTop)
-    console.log(plant.style.left)
-    console.log(plant.style.top)
-  }
-  // })
-
+  positions.forEach( position => {
+    const elmt = document.getElementById(`position_${position.id}`)
+    elmt.style.left = position.positionX + "px"
+    elmt.style.top  = position.positionY + "px"
+  })
 }
 
 export { listPlantPosition };
