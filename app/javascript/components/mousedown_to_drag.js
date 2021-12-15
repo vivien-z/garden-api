e9b8a634a42b8cef449d727b0a6ae1026c9b4584
vivien-z <yuanyuanzhu000@gmail.com>
2021-12-15 18:21:33 -0500

    cleaned code, noted important changes

--- app/javascript/components/mousedown_to_drag.js
+++ app/javascript/components/mousedown_to_drag.js
@@ -6,21 +6,6 @@ const mousedownToDrag = () => {
   const dropZone = document.getElementById(`drop_${garden.id}`)
 
   if (dragZone && dropZone) {
-
-  // ---- TEST: dropzone: getBoundingClientRect() -----------
-  // document.addEventListener( "scroll", () => {
-  //   let rectdpz = dropZone.getBoundingClientRect();
-  //   const exp = document.getElementById('example');
-  //   exp.innerHTML = ''; 
-  //   for (let key in rectdpz) {
-  //     if(typeof rectdpz[key] !== 'function') {
-  //       let para = document.createElement('p');
-  //       para.textContent  = `${ key } : ${ rectdpz[key] } `;
-  //       exp.appendChild(para);
-  //     }
-  //   }
-  // })
-  // console.log( dropZone.getBoundingClientRect())
   // ---------Define drag and drop: action--------------------------------
     dragZone.ondragstart = () => { return false }
     dropZone.ondragstart = () => { return false }
@@ -41,14 +26,7 @@ const mousedownToDrag = () => {
               shiftY = e.clientY - dragged.getBoundingClientRect().top;
               
         //------DRAG: new item to field------
-        if (isNewDrag) {
-          addToDropzone(dragged)
-          // const clone = duplicateDiv(dragged)
-          // dragged.parentNode.insertBefore(clone, dragged)
-          // dragged.classList.remove("plantOrigin", "yellow", "m-3")
-          // dragged.classList.add("plantCopy")
-          // dragged.id = plantCount
-        }
+        if (isNewDrag) setItemAtt(dragged)
         //-----------------------------------
         dragged.style.opacity = 0.7
         dragged.style.zIndex = 1000
@@ -58,7 +36,7 @@ const mousedownToDrag = () => {
         moveAt(dragged, e)
         document.addEventListener('mousemove', onMouseMove)
 
-        function addToDropzone(elmnt) {
+        function setItemAtt(elmnt) {
           const clone = duplicateDiv(elmnt)
           elmnt.parentNode.insertBefore(clone, elmnt)
           elmnt.classList.remove("plantOrigin", "yellow", "m-3")
@@ -78,7 +56,7 @@ const mousedownToDrag = () => {
         }
         //------DROP and remove un-needed handlers------
         dragged.onmouseup = function(e) {
-          const position = trackPosition(dragged)
+          const position = trackPosition(dragged) // capture getBoundingClientRect() value, to avoid abnormal data
           dragged.parentNode.removeChild(dragged)
           dropZone.appendChild(dragged)
           setPosition(position, dragged)
@@ -90,52 +68,48 @@ const mousedownToDrag = () => {
           addToPlantDetailList(dragged, plantCount, isNewDrag)
 
           dragged.onmouseup = null
-        }
-        function trackPosition(elmnt) {
-          const rectField = dropZone.getBoundingClientRect(),
-                rectElmnt = elmnt.getBoundingClientRect(),
-                position = { rectField: rectField, rectElmnt: rectElmnt }
-          return position
-        }
-        function setPosition(position, elmnt) {
-          const rectField = position.rectField,
-                rectElmnt = position.rectElmnt
-          const borderWth = 6
-
-          let isAllAdjusted = false
-
-          while (!isAllAdjusted) {
-            let tooLeft = rectElmnt.left < rectField.left + borderWth,
-                tooRight = rectElmnt.right > rectField.right - borderWth,
-                tooHigh = rectElmnt.top < rectField.top + borderWth,
-                tooLow = rectElmnt.bottom > rectField.bottom - borderWth
-            
-            elmnt.style.left = (rectElmnt.left - rectField.left - borderWth) + 'px'
-            elmnt.style.top = (rectElmnt.top - rectField.top - borderWth) + 'px'
-            if (tooLeft) {
-              elmnt.style.left = 0 + 'px'
-              // elmnt.style.left = borderWth + 'px'
-              tooLeft = false
+          function trackPosition(elmnt) {
+            const rectField = dropZone.getBoundingClientRect(),
+                  rectElmnt = elmnt.getBoundingClientRect(),
+                  position = { rectField: rectField, rectElmnt: rectElmnt }
+            return position
+          }
+          function setPosition(position, elmnt) {
+            const rectField = position.rectField,
+                  rectElmnt = position.rectElmnt
+            const borderWth = 6
+  
+            let isAllAdjusted = false
+            while (!isAllAdjusted) {
+              // set general element position
+              elmnt.style.left = (rectElmnt.left - rectField.left - borderWth) + 'px'
+              elmnt.style.top = (rectElmnt.top - rectField.top - borderWth) + 'px'
+              // adjust element position when dropped OUTSIDE range
+              let tooLeft = rectElmnt.left < rectField.left + borderWth,
+                  tooRight = rectElmnt.right > rectField.right - borderWth,
+                  tooHigh = rectElmnt.top < rectField.top + borderWth,
+                  tooLow = rectElmnt.bottom > rectField.bottom - borderWth
+              
+              if (tooLeft) {
+                elmnt.style.left = 0 + 'px'
+                tooLeft = false
+              }
+              if (tooRight) {
+                elmnt.style.left = (rectField.width - rectElmnt.width - 2*borderWth) + 'px'
+                tooRight = false
+              }
+              if (tooHigh) {
+                elmnt.style.top = 0 + 'px'
+                tooHigh = false
+              }
+              if (tooLow) {
+                elmnt.style.top = (rectField.height - rectElmnt.height - 2*borderWth) + 'px'
+                tooLow = false
+              } 
+              isAllAdjusted = ((tooLeft && tooRight && tooHigh && tooLow) === false)
             }
-            if (tooRight) {
-              elmnt.style.left = (rectField.width - rectElmnt.width - 2*borderWth) + 'px'
-              tooRight = false
-            }
-            if (tooHigh) {
-              elmnt.style.top = 0 + 'px'
-              // elmnt.style.top = (borderWth) + 'px'
-              tooHigh = false
-            }
-            if (tooLow) {
-              elmnt.style.top = (rectField.height - rectElmnt.height - 2*borderWth) + 'px'
-              tooLow = false
-            }
-            
-            isAllAdjusted = ((tooLeft && tooRight && tooHigh && tooLow) === false)
           }
         }
-
-
       }
     }
   }
