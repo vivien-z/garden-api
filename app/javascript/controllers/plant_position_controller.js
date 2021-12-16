35aae9d83d46fb3891f231b8ad857bf13d47839a
vivien-z <yuanyuanzhu000@gmail.com>
2021-12-16 12:11:00 -0500

    update Plants Detail Table count when new position created;use dataset to pass plantId to Stimulus Controller

--- app/javascript/controllers/plant_position_controller.js
+++ app/javascript/controllers/plant_position_controller.js
@@ -1,28 +1,30 @@
 import ApplicationController from './application_controller'
 
 export default class extends ApplicationController {
-  static targets = [ "plantInfo" ]
+  static targets = [ "plantCount" ]
 
-  add(event) {
+  submit(event) {
     event.preventDefault()
     const data = {
-      "garden_id": this.element.dataset.gardenId,
-      "plant_id": this.getInnerText("id"),
+      "garden_id": this.element.dataset.gardenId, //element: whole elmt the controller covers
+      "plant_id": event.target.dataset.plantId,
       "positionX": event.offsetX,
       "positionY": event.offsetY
     }
-    this.stimulate("GardenPlantForm#add_plant", event, event.target, data)
-    console.log(data)
+    this.stimulate("GardenPlantForm#add_plant", event, event.target, data);
+    // update Plants Detail table
+    const count = this.plantCountTarget.textContent
+    this.plantCountTarget.textContent = Number(count, 10) + 1
   }
   test(event) {
-    this.plantInfoTarget.textContent = 'Hello, Stimulus!'
-  }
-
-  getInnerText(data) {
-    const element = event.target.getElementsByClassName(`plant-detail__${data}`)[0]
-    if (element) {
-      return element.innerText
-    }
+    this.plantCountTarget.textContent = 'Hello, Stimulus!'
   }
 }
 
+// beforeReflex (element, reflex, noop, reflexId) {
+//   const { params } = element.reflexData[reflexId]
+//   element.reflexData[reflexId].params = {
+//     ...params, plant_position: true
+//   }
+//   console.log(element.reflexData[reflexId])
+// }
