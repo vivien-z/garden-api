d54edbcc294cfc69b3dc2eb4e4afe8cfca100393
vivien-z <yuanyuanzhu000@gmail.com>
2021-12-17 17:07:51 -0500

    pass and store elmnt position to Reflex

--- app/javascript/components/mousedown_to_drag.js
+++ app/javascript/components/mousedown_to_drag.js
@@ -3,81 +3,80 @@ import { setPlantPosition } from "../components/set_plant_position";
 
 const mousedownToDrag = () => {
   const garden = $('.garden_info').data('garden')
-  const dragZone = document.getElementById(`drag_${garden.id}`)
-  const dropZone = document.getElementById(`drop_${garden.id}`)
+  if (garden) {
+    const dragZone = document.getElementById(`drag_${garden.id}`)
+    const dropZone = document.getElementById(`drop_${garden.id}`)
 
-  if (dragZone && dropZone) {
-  // ---------Define drag and drop: action--------------------------------
-    dragZone.ondragstart = () => { return false }
-    dropZone.ondragstart = () => { return false }
+    if (dragZone && dropZone) {
+    // ---------Define drag and drop: action--------------------------------
+      dragZone.ondragstart = () => { return false }
+      dropZone.ondragstart = () => { return false }
 
-    dragZone.onmousedown = dragItem
-    dropZone.onmousedown = dragItem
+      dragZone.onmousedown = dragItem
+      dropZone.onmousedown = dragItem
 
-  // ---------Define drag event-------------------------------------------
-    function dragItem(e) {
-      e = e || window.event
-      e.preventDefault()
-      const dragged = e.target
-      const isNewDrag = dragged.parentNode.classList.contains("drag-zone")
-      let plantCount = 1 // XX
+    // ---------Define drag event-------------------------------------------
+      function dragItem(e) {
+        e = e || window.event
+        e.preventDefault()
+        const dragged = e.target
+        const isNewDrag = dragged.parentNode.classList.contains("drag-zone")
+        // let plantCount = 1 // XX
 
-      if (dragged && dragged.classList.contains("draggable")) {
-        const shiftX = e.clientX - dragged.getBoundingClientRect().left,
-              shiftY = e.clientY - dragged.getBoundingClientRect().top;
-              
-        //------DRAG: new item to field------
-        if (isNewDrag) setItemAtt(dragged)
-        //-----------------------------------
-        dragged.style.opacity = 0.7
-        dragged.style.zIndex = 1000
-        dragged.style.cursor = 'grabbing'
-        dragged.style.position = 'absolute'
-        
-        moveAt(dragged, e)
-        document.addEventListener('mousemove', onMouseMove)
-
-        function setItemAtt(elmnt) {
-          const clone = duplicateDiv(elmnt)
-          elmnt.parentNode.insertBefore(clone, elmnt)
-          elmnt.classList.remove("plantOrigin", "yellow", "m-3")
-          elmnt.classList.add("plantCopy")
-          elmnt.id = plantCount
-        }
-        function duplicateDiv(originDiv) {
-          let clone = originDiv.cloneNode(true)
-          return clone
-        }
-        function moveAt(elmnt, event) {
-          elmnt.style.left = event.pageX - shiftX + 'px'
-          elmnt.style.top = event.pageY - shiftY + 'px'
-        }
-        function onMouseMove(e) {
-          moveAt(dragged, e)
-        }
-        //------DROP and remove un-needed handlers------
-        dragged.onmouseup = function(e) {
-          const position = trackPosition(dragged, dropZone) // capture getBoundingClientRect() value, to avoid abnormal data
-          dragged.parentNode.removeChild(dragged)
-          dropZone.appendChild(dragged)
-          setPlantPosition(dragged, position.rectField, position.rectElmnt, 6)
-
-          // dragged.setAttribute("data-plant-positionx", `${dragged.style.left}`)
-          // dragged.setAttribute("data-plant-positiony", `${dragged.style.top}`)
-          // dragged.setAttribute("data-offset-position", `left: ${dragged.style.left}; top: ${dragged.style.top}`)
-
-          dragged.style.opacity = ''
-          dragged.style.cursor = 'grab'
-          document.removeEventListener('mousemove', onMouseMove)
-          plantCount = dropZone.getElementsByClassName('draggable').length
-          addToPlantDetailList(dragged, plantCount, isNewDrag)
+        if (dragged && dragged.classList.contains("draggable")) {
+          const shiftX = e.clientX - dragged.getBoundingClientRect().left,
+                shiftY = e.clientY - dragged.getBoundingClientRect().top;
+                
+          //------DRAG: new item to field------
+          if (isNewDrag) setItemAtt(dragged)
+          //-----------------------------------
+          dragged.style.opacity = 0.7
+          dragged.style.zIndex = 1000
+          dragged.style.cursor = 'grabbing'
+          dragged.style.position = 'absolute'
           
-          dragged.onmouseup = null
+          moveAt(dragged, e)
+          document.addEventListener('mousemove', onMouseMove)
 
-          function trackPosition(elmnt, dropZone) {
-            const rectField = dropZone.getBoundingClientRect(),
-                  rectElmnt = elmnt.getBoundingClientRect()
-            return { rectField: rectField, rectElmnt: rectElmnt }
+          function setItemAtt(elmnt) {
+            const clone = duplicateDiv(elmnt)
+            elmnt.parentNode.insertBefore(clone, elmnt)
+            elmnt.classList.remove("plantOrigin", "yellow", "m-3")
+            elmnt.classList.add("plantCopy")
+            // elmnt.id = plantCount
+          }
+          function duplicateDiv(originDiv) {
+            let clone = originDiv.cloneNode(true)
+            return clone
+          }
+          function moveAt(elmnt, event) {
+            elmnt.style.left = event.pageX - shiftX + 'px'
+            elmnt.style.top = event.pageY - shiftY + 'px'
+          }
+          function onMouseMove(e) {
+            moveAt(dragged, e)
+          }
+          //------DROP and remove un-needed handlers------
+          dragged.onmouseup = function(e) {
+            const position = trackPosition(dragged, dropZone) // capture getBoundingClientRect() value, to avoid abnormal data
+            dragged.parentNode.removeChild(dragged)
+            dropZone.appendChild(dragged)
+            setPlantPosition(dragged, position.rectField, position.rectElmnt, 6)
+            console.log(dragged.style.left)
+            console.log(dragged.style.top)
+            dragged.style.opacity = ''
+            dragged.style.cursor = 'grab'
+            document.removeEventListener('mousemove', onMouseMove)
+            // plantCount = dropZone.getElementsByClassName('draggable').length
+            // addToPlantDetailList(dragged, plantCount, isNewDrag)
+            
+            dragged.onmouseup = null
+
+            function trackPosition(elmnt, dropZone) {
+              const rectField = dropZone.getBoundingClientRect(),
+                    rectElmnt = elmnt.getBoundingClientRect()
+              return { rectField: rectField, rectElmnt: rectElmnt }
+            }
           }
         }
       }
--- app/javascript/controllers/plant_position_controller.js
+++ app/javascript/controllers/plant_position_controller.js
@@ -1,47 +1,26 @@
-import ApplicationController from './application_controller'
+import ApplicationController from './application_controller';
+import { setPlantPosition } from "../components/set_plant_position";
 
 export default class extends ApplicationController {
-  static targets = [ "plantCount" ]
+  // static targets = [ "plantCount" ]
 
   submit(event) {
     event.preventDefault()
+    // get plant position
+    const elmnt = event.target,
+          dropZone = document.getElementById(`drop_${this.element.dataset.gardenId}`),
+          rectField = dropZone.getBoundingClientRect(),
+          rectElmnt = elmnt.getBoundingClientRect()
+    setPlantPosition(elmnt, rectField, rectElmnt, 6)
+    console.log(elmnt)
+    console.log(elmnt.style.left)
+    console.log(elmnt.style.top)
     const data = {
       "garden_id": this.element.dataset.gardenId, //element: whole elmt the controller covers
       "plant_id": event.target.dataset.plantId,
-      "positionX": event.offsetX,
-      "positionY": event.offsetY
+      "positionX": elmnt.style.left,
+      "positionY": elmnt.style.top
     }
-    // this.stimulate("GardenPlantForm#add_plant", event, event.target, data);
-    // update Plants Detail table
-    const count = this.plantCountTarget.textContent
-    this.plantCountTarget.textContent = Number(count, 10) + 1
-    // get plant position
-    const dropZone = document.getElementById(`drop_${this.element.dataset.gardenId}`)
-    const rect = dropZone.getBoundingClientRect()
-    const shiftX = event.clientX - event.target.getBoundingClientRect().left,
-          shiftY = event.clientY - event.target.getBoundingClientRect().top;
-    const dataset = event.target.dataset
-    console.log(event.clientX - shiftX - rect.left)
-    console.log(event.clientY - shiftY - rect.top)
-
-    console.log(event.target.getAttribute("data-plant-positionx"))
-    console.log(event.target.getAttribute("data-plant-positionx"))
-    console.log(dataset.plantPositionx)
-    console.log(dataset.plantPositiony)
-    console.log(dataset.plantId)
-    console.log(dataset.action)
-    console.log(dataset)
-    console.log(event.target.attributes.style)
-    console.log(event.target)
-
+    this.stimulate("GardenPlantForm#add_plant", event, event.target, data);
   }
-
 }
-
-// beforeReflex (element, reflex, noop, reflexId) {
-//   const { params } = element.reflexData[reflexId]
-//   element.reflexData[reflexId].params = {
-//     ...params, plant_position: true
-//   }
-//   console.log(element.reflexData[reflexId])
-// }
